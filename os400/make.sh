7e1b09a2aafc0281d0659cd170e0fe022a519356
Chi-Iroh <69324626+Chi-Iroh@users.noreply.github.com>
2025-10-21 15:04:43 +0200

    [OS400] Renamed header with .rpgle extension

--- os400/make.sh
+++ os400/make.sh
@@ -239,8 +239,8 @@ done
 #       Install the ILE/RPG header file.
 
 
-HFILE="${SCRIPTDIR}/zlib.inc"
-MBR="ZLIB.INC"
+HFILE="${SCRIPTDIR}/zlibfixed.rpgle"
+MBR="ZLIBFIXED"
 DEST="${SRCPF}/${MBR}.MBR"
 
 if action_needed "${DEST}" "${HFILE}"
