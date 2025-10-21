6c3586e02a5eabb6ab328b4940d3de7850e8ceab
Chi-Iroh <69324626+Chi-Iroh@users.noreply.github.com>
2025-10-21 15:06:22 +0200

    [OS400] Renamed free form header

--- os400/make.sh
+++ os400/make.sh
@@ -249,7 +249,7 @@ then    CMD="CPY OBJ('${HFILE}') TOOBJ('${DEST}')"
         # touch -r "${HFILE}" "${DEST}"
 fi
 
-HFILE="${SCRIPTDIR}/zlibfree"
+HFILE="${SCRIPTDIR}/zlibfree.rpgle"
 MBR="ZLIBFREE"
 DEST="${SRCPF}/${MBR}.MBR"
 
